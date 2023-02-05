beef7ddca5131214c0a71f2fcc4a60fc378420d6
rushankhur <kazanface@gmail.com>
2023-02-05 11:46:58 -0400

    Add axios library. Add fetching repos from url

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,6 +1,8 @@
+import axios from 'axios';
 import { Router, Request, Response } from 'express';
 
 export const repos = Router();
+const GITHUB_REPOS_URL = 'https://api.github.com/users/silverorange/repos';
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
@@ -8,5 +10,10 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  const reposFromGithub = await axios({
+    method: 'GET',
+    url: GITHUB_REPOS_URL,
+  });
+
+  res.json(reposFromGithub.data);
 });
